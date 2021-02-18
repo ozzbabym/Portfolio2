46dbface2177fcbe8db0500f8f0e3047dc737887
Ivan Nazarov <yotayopta1@gmail.com>
2021-02-18 21:39:25 +0300

    first commit

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -14,8 +14,19 @@ export const Header = () => {
 
 
     const modalShow = () => {
-
-        $('#modal').slideToggle(500)
+        
+        if(document.getElementById('modal').style.transform==='translateX(0px)'){
+            document.getElementById('modal').style='z-index: -333;'
+            document.getElementById('modal').style='transform: translateX(2000px)'
+            
+            
+        }else{
+            document.getElementById('modal').style='z-index: 999;'
+            document.getElementById('modal').style='transform: translateX(0px)'
+            
+            
+        }
+        
        
     }
     
@@ -23,13 +34,28 @@ export const Header = () => {
     return (
         <div>
             <div className='header'>
-                <div>
+                <div className='js'>
                     JavaScript Developer
                 </div>
                 {window.innerWidth > 600 && <div>
-                    <button><NavLink to='/' style={{ color: "black" }} onClick={()=>{$('#modal').fadeOut(500); $('#modalFace').fadeOut(500)}}>About me</NavLink></button>
-                    <button><NavLink to='/projects' style={{ color: "black" }} onClick={()=>{$('#modal').fadeOut(500); $('#modalFace').fadeOut(500)}}>My projects</NavLink></button>
-                    <button onClick={()=>{$('#modalFace').fadeIn(500)}}>Contacts</button>
+                    <button ><NavLink className='btnAbout' to='/' style={{ color: "black" }} onClick={()=>{
+                        $('#modal').fadeOut(500) 
+                        $('#modalFace').fadeOut(500)
+                        $('.btnAbout').fadeOut(0)
+                        $('.btnAbout').fadeIn(300)
+                        }}>About me</NavLink></button>
+                    <button><NavLink className='btnProject' to='/projects' style={{ color: "black" }} onClick={()=>{
+                        $('#modal').fadeOut(500); 
+                        $('#modalFace').fadeOut(500)
+                        $('.btnProject').fadeOut(0)
+                        $('.btnProject').fadeIn(300)
+                        }}>My projects</NavLink></button>
+                    <button onClick={()=>{
+                        $('#modalFace').fadeIn(500)
+                        $('.btnContacts').fadeOut(0)
+                        $('.btnContacts').fadeIn(300)
+                        }    
+                        }><span className='btnContacts' >Contacts</span></button>
                 </div>}
                 {window.innerWidth < 600 &&
                     <div>
@@ -41,8 +67,8 @@ export const Header = () => {
                 }
                 <div className='modal' id='modal'>
                     <ul>
-                        <li><NavLink to='/' style={{ color: "white" }} onClick={()=>{$('#modal').fadeOut(500); $('#modalFace').fadeOut(500)}}>About me</NavLink></li>
-                        <li><NavLink to='/projects' style={{ color: "white" }}onClick={()=>{$('#modal').fadeOut(500); $('#modalFace').fadeOut(500)}}>My projects</NavLink></li>
+                        <li><NavLink to='/' style={{ color: "white" }} onClick={()=>{modalShow(); $('#modalFace').fadeOut(500)}}>About me</NavLink></li>
+                        <li><NavLink to='/projects' style={{ color: "white" }}onClick={()=>{modalShow(); $('#modalFace').fadeOut(500)}}>My projects</NavLink></li>
                         <li onClick={()=>{
                             $('#modalFace').fadeIn(500)
                             modalShow()
@@ -51,13 +77,13 @@ export const Header = () => {
                 </div>
 
                 <div id='modalFace' className='modalFace'>
-                    <div className='modalFace'>
+                    <div className='modalFaceIn'>
                         <ul>
                             <li>Имя: Иван</li>
                             <li>Телефон: 8-999-999-16-20</li>
                             <li>e-mail: OzzOzzz@mail.ru </li>
                             <li>город: Москва</li>
-                            <li><img src={photo}/></li>
+                            <li><img className='myPhoto' src={photo}/></li>
                             <div onClick={()=>$('#modalFace').fadeOut(500)}>Закрыть</div>
                         </ul>
                         
--- src/componets/Projects/Projects.jsx
+++ src/componets/Projects/Projects.jsx
@@ -8,15 +8,20 @@ import chat from '../../img/chat.png'
 import converter from '../../img/convertervalute.png'
 import shop from '../../img/internetshop.png'
 import gips from '../../img/gipsyard.jpg'
+import aviaTicket from '../../img/aviaticket.png'
+import changeTablet from '../../img/changeTablet.png'
 
 export const Projects = () => {
     let arrProjects = [
-        { id: 1, link: "https://ozzbabym.github.io/BallSquared/", photo: Ball, name: 'BallSquared', description: 'Игра для телефона, вращай квадрат, собирай синии кружки, не попадай в центр.' },
-        { id: 2, link: "", photo: Bot, name: 'Tennis Bot', description: 'Бот работает на сервере, парсит базу 1xBet, собирает нужную инфу и присылает сообщение в Телеграм' },
-        { id: 3, link: "https://ozzbabym.github.io/ChatMessages/#/", photo: chat, name: 'Chat Messages', description: 'Чат написан на React/Redux , сервер работает на NodeJs данные хранятся в MongoDB, не хватает webSocket.' },
-        { id: 4, link: "https://ozzbabym.github.io/Valuteconverter/#/", photo: converter, name: 'Converter Valute', description: 'Тестовое задание от работодателя, выводит через открытое API данные с валютами, можно добавить в избранное, на другой странице можно конвертировать валюту в рубли.' },
-        { id: 5, link: "https://ozzbabym.github.io/internetshop/", photo: shop, name: 'Internet Shop', description: 'Интернет магазин написан на React/Redux, есть категории товаров, можно добавить в корзину и в корзине оформить заказ, после можно увидеть в истории, историю заказов' },
-        { id: 6, link: "https://ozzbabym.github.io/shopshop/", photo: gips, name: 'GipsYard', description: 'Интернет магазин для продажи декортативных стен' }
+        
+        { id: 8, link: "https://ozzbabym.github.io/changeTablet/", photo: changeTablet, name: 'Change Tablet', description: 'Тестовое задание от работодателя, присудствует табло, выбираем номер ячейки, которая должна быть белой и в ней синяя цифра курсивом, остальные ячейки должны перекраситься в тот цвет, который выбран.' },
+        { id: 7, link: "https://ozzbabym.github.io/aviaticket/", photo: aviaTicket, name: 'Avia Ticket', description: 'Тестовое задание от работодателя, отображается список билетов и филтрация для них, билеты берутся из JSON файла.' },
+        { id: 1, link: "", photo: Bot, name: 'Tennis Bot', description: 'Бот работает на сервере, парсит базу 1xBet, собирает нужную инфу и присылает сообщение в Телеграм' },
+        { id: 2, link: "https://ozzbabym.github.io/ChatMessages/#/", photo: chat, name: 'Chat Messages', description: 'Чат написан на React/Redux , сервер работает на NodeJs данные хранятся в MongoDB, не хватает webSocket.' },
+        { id: 3, link: "https://ozzbabym.github.io/Valuteconverter/#/", photo: converter, name: 'Converter Valute', description: 'Тестовое задание от работодателя, выводит через открытое API данные с валютами, можно добавить в избранное, на другой странице можно конвертировать валюту в рубли.' },
+        { id: 4, link: "https://ozzbabym.github.io/internetshop/", photo: shop, name: 'Internet Shop', description: 'Интернет магазин написан на React/Redux, есть категории товаров, можно добавить в корзину и в корзине оформить заказ, после можно увидеть в истории, историю заказов' },
+        { id: 5, link: "https://ozzbabym.github.io/shopshop/", photo: gips, name: 'GipsYard', description: 'Интернет магазин для продажи декоративных стен' },
+        { id: 6, link: "https://ozzbabym.github.io/BallSquared/", photo: Ball, name: 'BallSquared', description: 'Игра для телефона, вращай квадрат, собирай синиe кружки, не попади в центр.' }
     ]
 
 
@@ -24,7 +29,7 @@ export const Projects = () => {
                 <div id='arr' className='projects' key={item.id}>
                     <div>{item.name}</div>
                     <div>{item.description}</div>
-                    <div><a href={item.link}>{item.link}</a></div>
+                        <div><a href={item.link}>жми сюда, перейти на {item.name}</a></div>
 
                     <img src={item.photo} />
 
@@ -33,7 +38,17 @@ export const Projects = () => {
 
       const [count, setCount] = useState(0)
 
+        const left=()=>{
+            setCount(count - 1)
+            $('.left').fadeOut(0)
+            $('.left').fadeIn(300)
+        }
+        const right=()=>{
+            setCount(count + 1)
+            $('.right').fadeOut(0)
+            $('.right').fadeIn(300)
 
+        }
 
 
 
@@ -46,7 +61,7 @@ export const Projects = () => {
                         <div className='projects' key={item.id}>
                             <div>{item.name}</div>
                             <div>{item.description}</div>
-                            <div><a href={item.link}>{item.link}</a></div>
+                            <div><a href={item.link}>жми сюда, перейти на {item.name}</a></div>
 
                             <img src={item.photo} />
 
@@ -57,9 +72,9 @@ export const Projects = () => {
             }
             {window.innerWidth > 600 &&
                 <div className='arrow'>{count >= 1 &&
-                    <button onClick={()=>{setCount(count - 1)}}>⇚</button>}
+                    <button className='left' onClick={left}>⇚</button>}
                     {arrMapProject.length-1>count &&
-                    <button onClick={()=>{setCount(count + 1)}}>⇛</button>}
+                    <button className='right' onClick={right}>⇛</button>}
 
                     {arrMapProject[count]}
                     
